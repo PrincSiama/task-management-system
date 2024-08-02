63bb91daea07d470b29b8a33c0be893e1a96cede
Сергей <sosnovskysy@yandex.ru>
2024-08-02 19:00:23 +0300

    start

---
+++ src/main/java/dev/sosnovsky/task/management/system/TaskManagementSystem.java
@@ -0,0 +1,12 @@
+package dev.sosnovsky.task.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementSystem.class, args);
+	}
+}
